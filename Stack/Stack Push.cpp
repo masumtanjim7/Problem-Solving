92b28c4d0ee8608cc3d87514ad3ded8ce8fb75a2
masumtanjim7 <masumbillah19999@gmail.com>
2023-03-23 22:17:25 +0600

    Masum-Stack Basic

---
+++ Stack/Stack Push.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+int main()
+{
+    stack <string> M;
+    M.push("Tanjim");
+    M.push("Zarin");
+    M.push("Nabil");
+    M.push("Masum");
+
+    cout<<M.top()<<endl;
+return 0;
+
+}
