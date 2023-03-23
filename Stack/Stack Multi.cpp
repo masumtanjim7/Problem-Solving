46e3f4988ee40512d5ffac41d72d2082c53daa01
masumtanjim7 <masumbillah19999@gmail.com>
2023-03-23 22:28:03 +0600

    Masum

---
+++ Stack/Stack Multi.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+int main()
+{
+    stack <string> M;
+
+    M.push("Zarin");
+    M.push("Masum");
+    M.push("Hasan");
+    M.push("Tanjim");
+
+    while(!M.empty())
+    {
+        string X;
+        X = M.top();
+        cout<< X <<endl;
+        M.pop();
+    }
+return 0;
+}
