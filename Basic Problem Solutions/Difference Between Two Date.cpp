cc5d5be7dd63503f13176b6f603f300b3043f194
masumtanjim7 <masumbillah19999@gmail.com>
2023-04-07 01:22:26 +0600

    Masum

---
+++ Basic Problem Solutions/Difference Between Two Date.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+
+struct Date {
+    int d, m, y;
+};
+
+const int monthDays[12]
+    = { 31, 28, 31, 30, 31, 30,
+       31, 31, 30, 31, 30, 31 };
